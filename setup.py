ffdceda695952041bf169616d0e32e2963fe292e
HappyDarling <linsyay@gmail.com>
2023-01-02 06:01:16 +0900

    Folder Structure Modification

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("./tkauto/README.md", "r") as fh:
+    long_description = fh.read()
+
+def get_requirements(path):
+    reqs = []
+    try:
+        with open(path, 'r') as f:
+            reqs =  f.read().split()
+        return reqs
+    except Exception:
+        return []
+
+setup(
+    name='tkauto',
+    version='1.0.0',
+    author='HappyDarling',
+    description='Tenkafuma Auto Recruitment KOR',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/HappyDarling/tkfmAutoRecruitment',
+    install_requires=get_requirements(path='./tkauto/requirements.txt'),
+    packages=find_packages(),
+    python_requires='==3.7.*',
+)
